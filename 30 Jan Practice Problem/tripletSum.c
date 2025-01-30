8728122706e3802ce21a0a5d425a4e1c6e6d402a
Ansh Bhargava <anshbhargava06@gmail.com>
2025-01-30 18:23:36 +0530

    Practice Problem on Linked List

--- 30 Jan Practice Problem/tripletSum.c
+++ 30 Jan Practice Problem/tripletSum.c
@@ -96,9 +96,9 @@ struct Node *createLinkedList(char *inputString)
     }
 
     struct Node *result = NULL;
-    for (int i = 0; i < iteratorJ; i++)
+    for (int iteratorI = 0; iteratorI < iteratorJ; iteratorI++)
     {
-        insertEnd(&result, data[i]);
+        insertEnd(&result, data[iteratorI]);
     }
 
     return result;
